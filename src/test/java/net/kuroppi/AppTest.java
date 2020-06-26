0702b919d17972dd580e4b4051cade02998d54a9
PC1247 <pc1247@pishi1247noMacBook-Pro.local>
2020-06-26 09:17:31 +0900

    mvn generate

---
+++ src/test/java/net/kuroppi/AppTest.java
@@ -0,0 +1,20 @@
+package net.kuroppi;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
